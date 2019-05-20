a37eaa41e184e5f8c54dbf5ead6db02e71c6ccd1
Joao Torres <joaoatorres@gmail.com>
2019-05-20 12:56:19 +0200

    Load seeds from data files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+[Merchant, Shopper, Order].each { |m| m.destroy_all }
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+p 'Loading Merchants'
+merchants = CSV.read('db/seed_record_files/merchants.csv')
+merchants.each do |m|
+  Merchant.create!(id: m[0].to_i, name: m[1], email: m[2], cif: m[3])
+end
+
+p 'Loading Shoppers'
+shoppers = CSV.read('db/seed_record_files/shoppers.csv')
+shoppers.each do |s|
+  Shopper.create!(id: s[0].to_i, name: s[1], email: s[2], nif: s[3])
+end
+
+p 'Loading Orders'
+orders = CSV.read('db/seed_record_files/orders.csv')
+orders.each do |o|
+  completed_at = o[4].present? ? DateTime.parse(o[4]) : nil
+
+  Order.create!(
+    id: o[0].to_i,
+    merchant_id: o[1],
+    shopper_id: o[2],
+    amount_cents: o[3].gsub('.','').to_i,
+    created_at: DateTime.parse(o[4]),
+    completed_at: completed_at
+  )
+end
